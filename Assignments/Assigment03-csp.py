41d306115c5bdfa502de78dab7bede4934f82544
LaisColetta <laiscoletta@gmail.com>
2023-10-20 13:52:40 +0100

    adding comments

--- Assignments/Assigment03-csp.py
+++ Assignments/Assigment03-csp.py
@@ -3,10 +3,17 @@ import json
 
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
 
+# Define a function to retrieve data from the web service
 def getAll():
+    # Send an HTTP GET request to the specified URL
     response = requests.get(url)
-    return response.json ()
+    # Return the JSON response from the web service
+    return response.json()
 
-if __name__=="__main__":
-    with open("cso.json","wt") as fp:
-        print (json.dumps(getAll()), file=fp)
+# Main code block
+if __name__ == "__main__":
+    # Open a file named "cso.json" in write text mode
+    with open("cso.json", "wt") as fp:
+        # Retrieve data from the web service using the 'getAll' function,
+        # convert it to a JSON string, and write it to the "cso.json" file.
+        print(json.dumps(getAll()), file=fp)
